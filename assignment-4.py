7c2263bbd3062f3c2bd89bfb92a042b7d03f82b9
saicharan4922 <114211653+saicharan4922@users.noreply.github.com>
2023-02-27 13:51:07 +0530

    map filter

---
+++ assignment-4.py
@@ -0,0 +1,23 @@
+add_25 = lambda num: num + 25
+result = add_25(10)
+print(result)  
+
+
+#2
+
+my_list = [1, 2, 3, 4, 5, 6, 7]
+
+triple_list = list(map(lambda x: x*3, my_list))
+
+print(triple_list) 
+
+
+#3
+
+my_list = [4, 5, 2, 9]
+
+square_list = list(map(lambda x: x**2, my_list))
+
+print(square_list) 
+
+
